e89ef9698b3ac5a297ed0cfe0f2288eabcf5397c
Nir Noy <nirnoy@Nirs-MacBook-Pro.local>
2015-02-07 19:27:52 +0200

    fixed list sample

--- Mod01/3_FileSystem/list.js
+++ Mod01/3_FileSystem/list.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
+var Path = require('path');
 var colors = require('colors');
 
 // this method reads the content of a directory
 fs.readdir(process.argv[2], function(err,files){
     files.forEach(function(file){
 
-        var path = process.argv[2] + '\\' + file;
+        var path = Path.join(process.argv[2],file);
 
         fs.stat(path, function(err, stats){
             if(stats != undefined){
