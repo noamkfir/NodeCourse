b658dbd2656067866f29fa1bc519ce917aa07794
nirnoy <noynir@gmail.com>
2015-03-02 00:19:26 +0200

    New Express Challenge

---
+++ Challenges/Express/ToDo/ToDoListChallenge/app/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+
--- Challenges/Express/ToDo/app/routes/tasks.js
+++ Challenges/Express/ToDo/app/routes/tasks.js
@@ -3,24 +3,33 @@ var taskService = require('../services/taskService');
 var Task = taskService.Task;
 var router = express.Router();
 
-router.get('/:state?',function(req,res,next){
+router.route("/")
+    .get(function(req,res,next){
 
-    var completed;
-    if(req.params.state)
-    {
-        completed = req.params.state==='completed';
-    }
+        var completed;
+        if(req.params.state)
+        {
+            completed = req.params.state==='completed';
+        }
 
-    taskService.getTasks(completed,function gotTasks(err,tasks){
-        res.send(tasks);
-    });
-});
+        taskService.getTasks(completed,function gotTasks(err,tasks){
+            res.send(tasks);
+        });
+    })
+    .put(function(req,res,next){
+        taskService.createTask(req.body.id,req.body.title,function gotTasks(err,task){
+            res.send(task);
+        });
+    })
+    .post(function(req,res,next){
+        taskService.update(req.body.tasks,function(err,tasks){
+           res.send(tasks);
+        });
+    })
+    .delete(function(req,res){
+        taskService.deleteTasks(req.body.tasks,function tasksDeleted(err,tasks){
+            res.send(tasks);
+        });
+    })
 
-router.put('/',function(req,res,next){
-
-    taskService.createTask(req.body.id,req.body.title,function gotTasks(err,task){
-        res.send(task);
-    });
-});
-
-module.exports  = router;
+module.exports=router;
