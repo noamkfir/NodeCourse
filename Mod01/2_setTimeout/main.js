7ef2c2687f06aed69d263f967425e0f5b4988fbf
Noam Kfir <noam@kfir.cc>
2016-05-15 05:46:36 +0300

    Cleaned up initial state of setTimeout demo.

--- Mod01/2_setTimeout/main.js
+++ Mod01/2_setTimeout/main.js
@@ -1,10 +1,9 @@
 var start = Date.now();
 // console.log('start: ' + start);
 
-    for(var i =0;i < 5000000000; i++){
-    }
 setTimeout(function() {
     console.log('Going once: ' + (Date.now() - start));
+    // for (var i = 0; i < 5000000000; i++);
 }, 1000);
 
 setTimeout(function() {
