93557da5db98252cd8de595e178e99f5aa826835
Noam Kfir <noam@kfir.cc>
2016-05-15 05:47:48 +0300

    Added fs pipe demo.

--- Mod01/3_FileSystem/pipes.js
+++ Mod01/3_FileSystem/pipes.js
@@ -1,3 +1,6 @@
 console.log('start reading files from console');
 
 process.stdin.pipe(process.stdout);
+
+// const fs = require('fs');
+// fs.createReadStream(process.argv[2]).pipe(process.stdout);
