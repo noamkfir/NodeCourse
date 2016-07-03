a6ba8f8fc7470a265ac563c5a527e9bc71642d09
Noam Kfir <noam@kfir.cc>
2016-07-03 17:28:11 +0300

    Updated the readFileSync demo to show the time span of the operation.

--- Mod01/3_FileSystem/readfilesync.js
+++ Mod01/3_FileSystem/readfilesync.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
+var start = Date.now();
 
 console.log('start reading file');
+
 var f = fs.readFileSync(process.argv[2], 'utf-8');
 console.log(f);
-console.log('done');
+
+console.log('done reading file after ' + (Date.now() - start));
