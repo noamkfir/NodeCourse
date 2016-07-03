9c2a0017e27eaaa060443907dfc67025533ce165
Noam Kfir <noam@kfir.cc>
2016-07-03 17:35:17 +0300

    Added http chunking demo for either gzip, or text with encoding.

--- Mod02/3_HttpChunked/chunked.js
+++ Mod02/3_HttpChunked/chunked.js
@@ -12,10 +12,22 @@ var handleRequests = function(req, res) {
     // creating an outgoing HTTP request
     var req2 = http.request(options, responseCallback = function(response) {
 
-        res.writeHead(200, {'content-type': 'text/xml', 'Content-Encoding': 'gzip'})
+        // chunk with gzip encoding
+        res.writeHead(200, {
+            'content-type': 'text/xml',
+            'Content-Encoding': 'gzip'
+        });
         response.on('data', function(chunk) {
             res.write(chunk);
         });
+
+        // chunk plain text with encoding
+        // res.writeHead(200, {
+        //     'content-type': 'text/xml'
+        // });
+        // response.on('data', function(chunk) {
+        //     res.write(chunk, 'utf8');
+        // });
 
         response.on('end', function() {
             res.end();
