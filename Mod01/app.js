e0213ed5f97bfee1b05431740797026bf6180e56
Noam Kfir <noam@kfir.cc>
2016-07-03 16:03:52 +0300

    Switched module to es6 syntax.

--- Mod01/app.js
+++ Mod01/app.js
@@ -1,8 +1,8 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.end('Hello, world!\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
